7037af8ef7020abfd3f9f6adf19204ae788743e8
m-ermolaev <lainiwakurafan@gmail.com>
2024-10-06 01:09:42 +0300

    Add styles

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  text-align: center;
+  color: rgb(61, 61, 61);
+  font-family: "Atlas Grotesk LC Web", sans-serif;
+`;
 
 function App() {
-  return <main>Maksim Ermolaev</main>;
+  return (
+    <Wrapper>
+      <main>
+        <div>
+          <h1>Maksim Ermolaev</h1>
+        </div>
+      </main>
+    </Wrapper>
+  );
 }
 
 const rootElement = document.createElement("div");
-rootElement.setAttribute("id", "app-root");
+rootElement.setAttribute("id", "app");
 
 document.body.appendChild(rootElement);
 
